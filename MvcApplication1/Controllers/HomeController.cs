904d45ef1d9f130b1a7b7075b3309c97fe7b0bb3
manishawattal <wattalmanisha@gmail.com>
2015-01-07 15:18:01 +0530

    my first mvc application for get and post

---
+++ MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewBag.dateshow = DateTime.Now;
+            return View();
+        }
+        [HttpGet]
+        public ActionResult elgibility()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult elgibility(Eligibile elgibile)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("thank You", elgibile);
+
+            }
+            else
+            {
+                return View();
+            }
+
+
+
+        }
+
+    }
+}
---
+++ MvcApplication1/Eligibile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1
+{
+    public class Eligibile
+    {
+        [Required(ErrorMessage="please enter the name")]
+        public string name { get; set; }
+        [Required(ErrorMessage="please specify your Age")]
+        public bool iseligibile { get; set; }
+    }
+}
