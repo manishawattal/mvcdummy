37fa2c34a147e54c8797dc894fda1ed80a4da25e
manishawattal <wattalmanisha@gmail.com>
2015-01-07 15:25:58 +0530

    commit1

--- MvcApplication1/Eligibile.cs
+++ MvcApplication1/Eligibile.cs
@@ -10,6 +10,8 @@ namespace MvcApplication1
     {
         [Required(ErrorMessage="please enter the name")]
         public string name { get; set; }
+        [Required(ErrorMessage = "please enter the name")]
+        public int age { get; set; }
         [Required(ErrorMessage="please specify your Age")]
         public bool iseligibile { get; set; }
     }
